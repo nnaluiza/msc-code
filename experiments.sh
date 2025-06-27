1569a0c79e0c257226b065a6e41f6700c9378964
Anna Luiza <anna.luiza.9919@gmail.com>
2025-06-27 15:21:08 -0300

    adjusting experiments to run on ubuntu

--- experiments.sh
+++ experiments.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-python script.py 1 40 4 euclidean iris
-python script.py 1 40 5 euclidean iris
-python script.py 1 40 6 euclidean iris
-python script.py 1 40 7 euclidean iris
+# python3 script.py 1 150 10 euclidean iris
+# python3 script.py 1 100 10 cosine iris
+# python3 script.py 1 100 10 cityblock iris
+python3 script.py 1 150 10 euclidean ds2c2sc13.arff
+# python3 script.py 1 50 10 cosine 2sp2glob.arff
+# python3 script.py 1 50 10 cityblock 2sp2glob.arff
+# python3 script.py 1 50 10 euclidean complex8.arff
+# python3 script.py 1 50 10 cosine complex8.arff
+# python3 script.py 1 50 10 cityblock complex8.arff
+# python3 script.py 1 50 10 euclidean complex9.arff
+# python3 script.py 1 50 10 cosine complex9.arff
+# python3 script.py 1 50 10 cityblock complex9.arff
